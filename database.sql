66a40055faca1b3d94085fecfa4dded367e74f29
eagudelo-cell <eagudelo@gmail.com>
2026-05-04 19:46:13 -0500

    qq

--- database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE envios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    codigo VARCHAR(50) NOT NULL,
-    descripcion TEXT,
-    destino VARCHAR(100) NOT NULL
-);
--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("db.php");
-
-$id = $_GET['id'];
-
-$conn->query("DELETE FROM envios WHERE id=$id");
-
-header("Location: index.php");
-?>
